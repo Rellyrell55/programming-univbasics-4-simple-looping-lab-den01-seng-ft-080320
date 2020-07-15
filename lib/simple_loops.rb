39f27de5760086cbc684713bf6ff06bbb444a596
Terrell Cooper <terrellcoop@gmail.com>
2020-07-15 20:24:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,4 +6,11 @@ def loop_message_five_times(message)
   end 
 end
 
-def 
+def loop_message_n_times (message, limit)
+  count = 0 
+  while count < limit do 
+    puts message
+    count += 1 
+  end 
+end 
+  
